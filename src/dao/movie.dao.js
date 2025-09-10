9cce4d343929501c3e68b251baf15a85585de031
KyanTsoi <kyantsoi1@gmail.com>
2025-09-10 12:46:04 +0200

    naam config folder aangepast naar database

--- src/dao/movie.dao.js
+++ src/dao/movie.dao.js
@@ -1,11 +1,6 @@
-const pool = require('../config/database');
+const pool = require('../database/database');
 
-/**
- * Haalt alle films (films) op uit de database.
- */
 function getAllMovies(callback) {
-    // We selecteren alleen de kolommen die we nodig hebben.
-    // 'LIMIT 20' is een goede gewoonte om te voorkomen dat je per ongeluk duizenden rijen ophaalt.
     const query = 'SELECT film_id, title, description, release_year FROM film LIMIT 20';
 
     pool.query(query, (error, results) => {
