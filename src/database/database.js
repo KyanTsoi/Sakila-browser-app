f497e5d3ab72a7c3ee6bab37966b36748b68ed7c
KyanTsoi <kyantsoi1@gmail.com>
2025-09-15 11:00:01 +0200

    dotenv volledig geinitialiseerd

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,25 @@
-const mysql = require('mysql2');
+require('dotenv').config(); // Laad variabelen uit .env bestand
 
+const mysql = require('mysql2');
+const logger = require('../util/logger');
+const { json } = require('express');
 // Maak een connection pool aan. Een pool is efficiënter dan een losse connectie
 // omdat het verbindingen hergebruikt.
+
+logger.debug("Database connectie info:")
+logger.debug({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+});
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || DB_HOST,
-  user: process.env.DB_USER || DB_USER,
-  password: process.env.DB_PASSWORD || DB_PASSWORD,
-  database: process.env.DB_DATABASE || DB_DATABASE,
-  port: process.env.DB_PORT || DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
